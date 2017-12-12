6fb475391feba14d4592764ac9ee8b2d70321731
vioif <33756966+vioif@users.noreply.github.com>
2017-12-11 21:05:09 -0500

    Update exercise3.rb

--- exercise3.rb
+++ exercise3.rb
@@ -3,35 +3,3 @@ puts "What is your name?"
 user_name = gets
 
 puts "Hello, #{user_name}"
-
-if my_number > 1
-
-puts "The number is greater than 1"
-end
-
-number = gets.to_i # the user types in a number
-
-if number > 0
-  puts "#{number} is positive" # this line executes if the user enters a positive number
-else
-  puts "#{number} is negative" # this line executes if the user enters a negative number
-end
-
-x = gets.to_i
-y = gets.to_i
-
-if x > y
-  puts "x is greater than y!"
-elsif x < y
-  puts "x is less than y!"
-else
-  puts "x equals y!"
-end
-
-if x != 10
-  puts "I get printed!"
-end
-
-unless x == 10
-  puts "I get printed!"
-end
